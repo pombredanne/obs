4d9287e29e553adb28a04869cd6daba0a5b9f537
Dan Kegel <dank@kegel.com>
2019-01-24 11:06:25 -0800

    buildbot/slaves.sh: ignore comments

--- buildbot/slaves.sh
+++ buildbot/slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Get a list of the slaves from master.json
 set -e
-sed -e '1,/"slaves"/d' -e '/]/,$d' < master.json | sed 's/.*:"//;s/".*//' | sort -u
+sed -e '1,$s/, "comment.*//' -e '1,/"slaves"/d' -e '/]/,$d' < master.json | sed 's/.*:"//;s/".*//' | sort -u
